84d5d83cccdc2bdbb148ec3f054d3bed9a766724
septlog <septlog@qq.com>
2021-04-05 17:53:23 +0800

    init.

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// router
+const indexRouter = require('./routes/index');
+const port = 3000;
+
+app.use('/', indexRouter);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
